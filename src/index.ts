d98f7b62f43fa51864a0c637834f4829fd7229db
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-10-28 20:16:58 -0700

    add web server with http module

--- src/index.ts
+++ src/index.ts
@@ -95,9 +95,20 @@ class HomeController extends Router {
   }
 }
 
-// change this to call router directly
+// WEB SERVER
+import http from "http";
+const host = "localhost";
+const port = 8000;
 
-// ** APP LISTENING ON PORT 3000 **
+const requestListener = function(req: any, res: any) {
+  res.writeHead(200);
+  res.end("My first server!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
 
 // app receives http request
 // handle request using the Router
